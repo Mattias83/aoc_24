8d58849c595ec77f22ab97c60cf1bcec2eb1f1db
Mattias83 <qvintus@gmail.com>
2024-12-01 09:49:43 +0100

    day1

---
+++ day1/part1.py
@@ -0,0 +1,21 @@
+def main():
+    # Load puzzle_input.txt into a list of lines.
+    with open("puzzle_input.txt", "r") as pz:
+        lines = pz.read().splitlines()
+
+    # reads in left_list & right_list and sorts them.
+    left_list, right_list = map(
+        lambda lst: sorted(map(int, lst)),
+        zip(*[line.split("   ") for line in lines]),
+    )
+
+    # calculate differnce and adds them togheter.
+    total_difference = sum(
+        abs(left - right) for left, right in zip(left_list, right_list)
+    )
+
+    print(total_difference)
+
+
+if __name__ == "__main__":
+    main()
---
+++ day1/part2.py
@@ -0,0 +1,17 @@
+def main():
+    with open("puzzle_input.txt", "r") as pz:
+        lines = pz.read().splitlines()
+
+        # Using zip to unpack the columns in puzzle_input to left_list and right_list
+        left_list, right_list = map(
+            lambda lst: list(map(int, lst)),
+            zip(*[line.split("   ") for line in lines]),
+        )
+
+        # Multiply left_list value with number of instances in rightlist and adds the result.
+        total = sum(left * right_list.count(left) for left in left_list)
+        print(total)
+
+
+if __name__ == "__main__":
+    main()
